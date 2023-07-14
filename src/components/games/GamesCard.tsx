4377114ab601b83fa694a9ddae6b6ed72367586d
ayakuma309 <ayako.30901@gmail.com>
2023-07-14 23:44:31 +0900

    feat: 詳細画面へのリンクを追加

--- src/components/games/GamesCard.tsx
+++ src/components/games/GamesCard.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { GamesCardProps } from '@/types/Type'
+import Link from 'next/link'
 
-const GamesCard:React.FC<GamesCardProps> = ({cover, name}) => {
+const GamesCard:React.FC<GamesCardProps> = ({id, cover, name, slug}) => {
   return (
     <div>
       <div className="flex flex-col justify-center items-center">
         <img
           src={cover ? cover.url.replace('thumb', 'cover_big') : '/no-image.png'}
           alt="cover"
-          className="h-44 w-1/2 mx-auto rounded-md relative overflow-hidden"
+          className="h-44 w-1/2 mx-auto rounded-md"
         />
         <h2 className="text-center text-lg font-bold">{name}</h2>
+      </div>
+      <div className="flex justify-center">
+        <Link href={{ pathname: `/games/${slug}`, query: { id: id } }}>
+          <button className="p-2 font-bold outline">
+            このゲーム好き！
+          </button>
+        </Link>
       </div>
     </div>
   )
 }
 
-export default GamesCard
+export default memo(GamesCard)
